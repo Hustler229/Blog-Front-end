3c97b7e40634b5cfe7b497b5112424fe873ebf85
Igor Adande <igoradande44@gmail.com>
2024-06-28 05:11:18 +0200

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,12 @@ export default function Home() {
   return (
     <>
       <h2 className="text-lg">Next App</h2>
-      <div className="flex gap-4 justify-between items-center">
+      <div className="flex flex-col gap-4 justify-between items-center">
         <p className="text-red-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta obcaecati doloremque repudiandae ut aliquid corporis, ex temporibus debitis enim quas non, sed iste distinctio quae laboriosam consectetur, officiis fuga odit!</p>
         <p className="text-yellow-900">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta obcaecati doloremque repudiandae ut aliquid corporis, ex temporibus debitis enim quas non, sed iste distinctio quae laboriosam consectetur, officiis fuga odit!</p>
+        <p className="text-green-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta obcaecati doloremque repudiandae ut aliquid corporis, ex temporibus debitis enim quas non, sed iste distinctio quae laboriosam consectetur, officiis fuga odit!</p>
+        <p className="text-green-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta obcaecati doloremque repudiandae ut aliquid corporis, ex temporibus debitis enim quas non, sed iste distinctio quae laboriosam consectetur, officiis fuga odit!</p>
+        <p className="text-green-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta obcaecati doloremque repudiandae ut aliquid corporis, ex temporibus debitis enim quas non, sed iste distinctio quae laboriosam consectetur, officiis fuga odit!</p>
         <p className="text-green-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta obcaecati doloremque repudiandae ut aliquid corporis, ex temporibus debitis enim quas non, sed iste distinctio quae laboriosam consectetur, officiis fuga odit!</p>
       </div>
     </>
